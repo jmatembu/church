30bb5c0abca5d06504f0f45f8c95b68032a87914
Joseph M. Matembu <jmatembu@gmail.com>
2019-05-01 12:26:32 +0300

    Add parish home link to admin menu

--- resources/views/parish/layout/partials/admin-sidebar.blade.php
+++ resources/views/parish/layout/partials/admin-sidebar.blade.php
@@ -11,6 +11,11 @@
 		</div>
 	</div>
 	<ul class="side-menu">
+        <li>
+            <a class="side-menu__item" href="{{ route('parish.index') }}">
+                <i class="side-menu__icon fe fe-home"></i><span class="side-menu__label">Parish Home</span>
+            </a>
+        </li>
 		<li>
 			<a class="side-menu__item"  href="{{ route('parish.admin.dashboard', $parish) }}"><i class="side-menu__icon fe fe-globe"></i><span class="side-menu__label">Dashboard</span></a>
 		</li>
